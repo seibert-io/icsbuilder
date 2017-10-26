27b1d01b6a09a0368cb98f2a07319495a47a8e84
David Seibert <d.seibert@s-v.de>
2017-10-26 11:41:31 +0200

    changed absolute asset loading to relative path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/vue/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: './',
         filename: 'main.js'
     },
     module: {
